1e9a238e501f82ca70c8d0ea5490ed17d47c591e
Mateus Rissi <mtrissi@gmail.com>
2020-09-07 23:17:12 -0300

    testing bg

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import React, { Component } from "react";
 import Header from "./components/header/header.js";
 import Main from "./main.js";
 import Footer from "./components/footer/footer.js";
+import test from "./images/background.png";
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div
+        style={{
+          backgroundImage: `url(${test})`,
+          backgroundRepeat: "no-repeat",
+        }}
+      >
         <Header />
         <Main />
         <Footer />
--- src/components/landing_page/landing_page.js
+++ src/components/landing_page/landing_page.js
@@ -1,54 +1,43 @@
 import React from "react";
-import { Row, Col, Image, Button } from "react-bootstrap";
+import { Row, Col, Image, Button, Container } from "react-bootstrap";
 
 function LandingPage() {
   return (
-    <Row>
-      <Col lg={{ order: 2 }} className="my-auto text-center">
-        <h1>O EMBAIXADOR</h1>
-        <p>
-          I'm baby hammock viral woke pinterest bushwick, tacos polaroid
-          succulents air plant beard bicycle rights fam four loko glossier small
-          batch. Roof party godard taiyaki sustainable craft beer. Deep v godard
-          live-edge, DIY microdosing pork belly stumptown locavore chartreuse
-          typewriter. Truffaut sartorial pug iceland plaid bushwick trust fund,
-          heirloom pabst mumblecore master cleanse synth wayfarers tattooed.
-          Poke affogato bitters literally.
-        </p>
-        <hr />
-        <Row className="text-center">
-          <Col>
-            <Button href="#">Spotify</Button>
-          </Col>
-          <Col>
-            <Button href="#">Deezer</Button>
-          </Col>
-        </Row>
-        <Row className="text-center">
-          <Col>
-            <Button href="#">Youtube</Button>
-          </Col>
-          <Col>
-            <Button href="#">Spotify</Button>
-          </Col>
-        </Row>
-        <Row className="text-center">
-          <Col>
-            <Button href="#">Spotify</Button>
-          </Col>
-          <Col>
-            <Button href="#">Spotify</Button>
-          </Col>
-        </Row>
-      </Col>
-      <Col lg={{ order: 1 }}>
-        <Image
-          src={require("../../images/warrior.png")}
-          alt="warrior image"
-          fluid
-        />
-      </Col>
-    </Row>
+    <Container>
+      <Row className="my-auto text-center">
+        <Col lg={{ order: 2 }} className="text-center">
+          <Image
+            src={require("../../images/game_title.png")}
+            alt="O Embaixador"
+            fluid
+          />
+          <p>
+            I'm baby hammock viral woke pinterest bushwick, tacos polaroid
+            succulents air plant beard bicycle rights fam four loko glossier
+            small batch. Roof party godard taiyaki sustainable craft beer. Deep
+            v godard live-edge, DIY microdosing pork belly stumptown locavore
+            chartreuse typewriter. Truffaut sartorial pug iceland plaid bushwick
+            trust fund, heirloom pabst mumblecore master cleanse synth wayfarers
+            tattooed. Poke affogato bitters literally.
+          </p>
+          <Row className="text-center pt-5">
+            <Col>
+              <Button href="#">Spotify</Button>
+            </Col>
+            <Col>
+              <Button href="#">Deezer</Button>
+            </Col>
+          </Row>
+        </Col>
+        <Col lg={{ order: 1 }}>
+          <Image
+            src={require("../../images/warrior.png")}
+            alt="warrior image"
+            fluid
+          />
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
